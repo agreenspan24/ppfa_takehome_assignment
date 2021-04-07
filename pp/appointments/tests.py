9b6c2164052c323ccb9985f6720b26ea236f8ae4
Adam Greenspan <agreenspan24@gmail.com>
2021-04-07 14:33:38 -0400

    Appointments

--- pp/appointments/tests.py
+++ pp/appointments/tests.py
@@ -1,23 +1,166 @@
 from django.test import TestCase
 import requests
 from .models import Appointment
+from datetime import datetime, timedelta
 
 class AppointmentTest(TestCase):
+    def create_appointment(self, data= {}):
+        base_data = {
+            "datetime": datetime.now(),
+            "reason": "GC",
+            "new_patient": True,
+            "contact_phone_number": "(214)543-2542"
+        }
+
+        merged_data = {**base_data, **data}
+
+        response = requests.post("http://localhost:8000/api/v1/appointments/", data= merged_data)
+
+        if response.status_code != 201:
+            return None
+
+        return response.json()
+
+
+    def delete_appointment(self, id):
+        response = requests.delete(f"http://localhost:8000/api/v1/appointments/{id}")
+
+        if response.status_code != 200:
+            return None
+
+
+    def get_appointment(self, id):
+        response = requests.get(f"http://localhost:8000/api/v1/appointments/{id}")
+
+        if response.status_code != 200:
+            return None
+
+        return response.json()
+
+
+    def get_appointments(self, start_date= None, end_date= None):
+        response = requests.get("http://localhost:8000/api/v1/appointments", params= {
+            "start_date": start_date,
+            "end_date": end_date
+        })
+
+        if response.status_code != 200:
+            return None
+
+        return response.json()
+
+
+    def update_appointment(self, id, data):
+        response = requests.post(f"http://localhost:8000/api/v1/appointments/{id}", data= data)
+
+        if response.status_code != 200:
+            return None
+
+        return response.json()
+
+
+    def delete_all_appointments(self):
+        appointments = self.get_appointments()
+
+        for appt in appointments:
+            self.delete_appointment(appt["id"])
+
+
     def setUp(self):
         pass
 
+
     def tearDown(self):
-        pass
+        self.delete_all_appointments()
+
 
     def test_get_appointments(self):
-        response = requests.get('http://localhost:8000/api/v1/appointments')
-        pass
+        appt_one = self.create_appointment()
+        appt_two = self.create_appointment()
+
+        server_appointments = self.get_appointments()
+
+        self.assertIsNotNone(appt_one)
+        self.assertIsNotNone(appt_two)
+        self.assertIsNotNone(server_appointments)
+
+        server_appt_ids = list(map(lambda a: a["id"], server_appointments))
+
+        self.assertEqual(len(server_appt_ids), 2, "More appointments exist on server than created")
+        self.assertIn(appt_one["id"], server_appt_ids, "Appointment one not in list from server")
+        self.assertIn(appt_two["id"], server_appt_ids, "Appointment two not in list from server")
+
 
     def test_create_appointment(self):
-        pass
+        appt = self.create_appointment()
+
+        self.assertIsNotNone(appt, "Appointment was not created")
+
 
     def test_delete_appointment(self):
-        pass
+        appt = self.create_appointment()
+        self.assertIsNotNone(appt)
+
+        self.delete_appointment(appt["id"])
+
+        all_appointments = self.get_appointments()
+
+        self.assertIsNotNone(all_appointments)
+        self.assertNotIn(appt["id"], map(lambda a: a["id"], all_appointments), "Appointment was not deleted")
+
 
     def test_get_appointment_by_pk(self):
-        pass
+        appt = self.create_appointment()
+        self.assertIsNotNone(appt)
+
+        fetched_appt = self.get_appointment(appt["id"])
+
+        self.assertIsNotNone(fetched_appt)
+        self.assertEqual(appt["id"], fetched_appt["id"])
+        self.assertEqual(appt["datetime"], fetched_appt["datetime"])
+        self.assertEqual(appt["reason"], fetched_appt["reason"])
+        self.assertEqual(appt["new_patient"], fetched_appt["new_patient"])
+        self.assertEqual(appt["contact_phone_number"], fetched_appt["contact_phone_number"])
+
+
+    def test_update_appointment(self):
+        appt = self.create_appointment()
+        self.assertIsNotNone(appt)
+
+        appt_updates = {
+            "datetime": datetime.now() + timedelta(hours= 2),
+            "reason": "CH",
+            "new_patient": False
+        }
+
+        updated_appt = self.update_appointment(appt["id"], appt_updates)
+        self.assertIsNotNone(updated_appt)
+
+        fetched_appt = self.get_appointment(appt["id"])
+        self.assertIsNotNone(fetched_appt)
+
+        self.assertEqual(fetched_appt["id"], appt["id"])
+        self.assertEqual(fetched_appt["datetime"], updated_appt["datetime"])
+        self.assertEqual(fetched_appt["reason"], updated_appt["reason"])
+        self.assertEqual(fetched_appt["new_patient"], updated_appt["new_patient"])
+        self.assertEqual(fetched_appt["contact_phone_number"], appt["contact_phone_number"])
+
+
+    def test_get_appointments_filtered(self):
+        appt_today = self.create_appointment()
+        appt_next_week = self.create_appointment({
+            "datetime": datetime.now() + timedelta(days= 7)
+        })
+
+        self.assertIsNotNone(appt_today)
+        self.assertIsNotNone(appt_next_week)
+
+        fetched_appts = self.get_appointments(
+            start_date= datetime.now().strftime("%m-%d-%Y"),
+            end_date= (datetime.now() + timedelta(days= 2)).strftime("%m-%d-%Y")
+        )
+
+        self.assertIsNotNone(fetched_appts)
+        self.assertIn(appt_today["id"], map(lambda a: a["id"], fetched_appts))
+        self.assertNotIn(appt_next_week["id"], map(lambda a: a["id"], fetched_appts))
+
--- pp/appointments/views.py
+++ pp/appointments/views.py
@@ -1,5 +1,10 @@
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+
+from datetime import datetime
+from pytz import timezone
 
 from .serializers import AppointmentSerializer
 from .models import Appointment
@@ -8,27 +13,97 @@ class GetDeleteUpdateAppointments(RetrieveUpdateAPIView):
     serializer_class = AppointmentSerializer
 
     def get_queryset(self, pk):
-        pass
+        return Appointment.objects.get(pk= pk)
 
     def get(self, request, pk):
-        pass
+        appointment = None
+
+        try:
+            appointment = self.get_queryset(pk)
+
+        except:
+            return Response("Appointment does not exist", status= status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(appointment)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, pk):
-        pass
+        appointment = None
+
+        try:
+            appointment = self.get_queryset(pk)
+
+        except:
+            return Response("Appointment does not exist.", status= status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(
+            appointment,
+            data= request.data,
+            partial= True
+        )
+
+        try:
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+        except ValidationError as e:
+            return Response(e.detail, status= status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, pk):
-        pass
+        appointment = None
+
+        try:
+            appointment = self.get_queryset(pk)
+
+        except:
+            return Response("Appointment does not exist.", status= status.HTTP_404_NOT_FOUND)
+
+        appointment.delete()
+
+        return Response(status= status.HTTP_200_OK)
+
 
 class GetPostAppointments(ListCreateAPIView):
     serializer_class = AppointmentSerializer
 
     def get_queryset(self):
-        pass
+        appointments = Appointment.objects.all()
+
+        start_date = self.request.query_params.get("start_date", None)
+        end_date = self.request.query_params.get("end_date", None)
+
+        if start_date:
+            start_date_parsed = timezone("UTC").localize(datetime.strptime(start_date, "%m-%d-%Y"))
+            appointments = appointments.filter(datetime__gte= start_date_parsed)
+
+        if end_date:
+            end_date_parsed = timezone("UTC").localize(datetime.strptime(end_date, "%m-%d-%Y"))
+            appointments = appointments.filter(datetime__lte= end_date_parsed)
+
+        return appointments
 
     def get(self, request):
-        pass
+        appointments = self.get_queryset()
+
+        serializer = self.serializer_class(appointments, many= True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        pass
+        data = request.data
+
+        serializer = self.serializer_class(data=data)
+
+        try:
+            serializer.is_valid(raise_exception= True)
+            serializer.save()
+
+        except ValidationError as e:
+            return Response(e.detail, status= status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
 
     
